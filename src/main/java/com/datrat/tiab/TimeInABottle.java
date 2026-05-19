0b5dc61fe20cfb8e333afe48f43a859d7685cf2a
DatRatVS <ricorian2006@gmail.com>
2026-05-18 21:26:45 -0300

    chore: rename mod to Rewinded

--- src/main/java/com/datrat/tiab/TimeInABottle.java
+++ src/main/java/com/datrat/tiab/TimeInABottle.java
@@ -22,7 +22,7 @@ import net.minecraft.item.ItemStack;
 @Mod(modid = TimeInABottle.MOD_ID, name = TimeInABottle.MOD_NAME, version = TimeInABottle.VERSION)
 public class TimeInABottle {
     public static final String MOD_ID = "tiab";
-    public static final String MOD_NAME = "Time In A Bottle";
+    public static final String MOD_NAME = "Time in a Bottle: Rewinded";
     public static final String VERSION = "1.0.1";
 
     @Mod.Instance(MOD_ID)
